f6b2e7a913be6e062d031abf5472a24065aa607a
Gonxa <Gonza@example.com>
2021-12-21 02:15:18 -0300

    Estilizado inicio

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,10 @@
 from django.db.models import fields
+from django.forms import forms
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, PostView, Like, Comment
+from .forms import PostForm
+
 
 class PostListView(ListView):
 	model = Post
@@ -10,17 +13,29 @@ class PostDetailView(DetailView):
 	model = Post
 	
 class PostCreateView(CreateView):
+	form_class = PostForm
 	model = Post
+	success_url = '/'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context.update ({
+			'view_type': 'create'
+		})
+		return context
+	
 
 class PostUpdateView(UpdateView):
+	form_class = PostForm
 	model = Post
-	fields = (
-		'title',
-		'content',
-		'thumbnail',
-		'author',
-		'slug'
-	)
+	success_url = '/'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context.update({
+			'view_type': 'update'
+		})
+		return context
 
 class PostDeleteView(DeleteView):
 	model = Post
